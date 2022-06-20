394d07e0ce09bc7caeca096f757abff5fcaae138
Hunter Sutton <suttonhu@oregonstate.edu>
2022-06-19 20:14:25 -0700

    Site now uses handlebars to render content

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var path = require('path');
+var exphbs = require('express-handlebars');
 
 var app = express();
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -13,7 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '/public/pages/index.html'));
+  res.status(200).render('home');
 });
 
 app.get('/style.css', (req, res) => {
@@ -23,7 +27,7 @@ app.get('/style.css', (req, res) => {
 
 // catch 404 and forward to error handler
 app.get('*', (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, '/public/pages/404.html'));
+  res.status(404).render('404');
 });
 
 module.exports = app;
